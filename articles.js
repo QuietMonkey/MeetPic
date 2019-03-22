cf2468fa596391901171f28489c9af429956068b
QuietMonkey <taillandierbertrand@gmail.com>
2019-03-22 11:12:07 +0100

    merge non fonctionnelle

--- articles.js
+++ articles.js
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8470c9aa37a4550e7eec087038bd9bfaced757f2
 let modal = document.getElementById('myModal');
 
 let btn = document.getElementById("myBtn");
@@ -8,14 +11,24 @@ let span = document.querySelector('.close');
 btn.onclick = function() {
   modal.style.display = "block";
 }
+<<<<<<< HEAD
 
 
 span.onclick = _ => modal.style.display = 'none';
 
 
 
+=======
+span.onclick = _ => modal.style.display = 'none';
+>>>>>>> 8470c9aa37a4550e7eec087038bd9bfaced757f2
 window.onclick = function(event) {
   if (event.target === span) {
     modal.style.display = "none";
   }
+<<<<<<< HEAD
 } 
+=======
+} 
+
+
+>>>>>>> 8470c9aa37a4550e7eec087038bd9bfaced757f2
