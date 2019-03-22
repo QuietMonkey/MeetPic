8470c9aa37a4550e7eec087038bd9bfaced757f2
manonab <abelcoindozm@gmail.com>
2019-03-22 09:46:36 +0100

    3D cards ok ARTICLES

--- articles.js
+++ articles.js
@@ -1,4 +1,3 @@
-
 let modal = document.getElementById('myModal');
 
 let btn = document.getElementById("myBtn");
@@ -8,14 +7,11 @@ let span = document.querySelector('.close');
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
-
 span.onclick = _ => modal.style.display = 'none';
-
-
-
 window.onclick = function(event) {
   if (event.target === span) {
     modal.style.display = "none";
   }
 } 
+
+
